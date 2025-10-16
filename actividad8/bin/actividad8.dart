111431622f1cfdb460de5eac7f8096d6bebcede1
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-10-16 13:59:29 +0200

    haha

--- actividad8/bin/actividad8.dart
+++ actividad8/bin/actividad8.dart
@@ -1,5 +1,5 @@
-
-
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 /*
 Descarga una imagen desde una URL usando la librería http y guarda el archivo en el
@@ -7,9 +7,16 @@ sistema de archivos. Asegúrate de utilizar await para esperar la descarga antes
 continuar.
  */
 void main() async {
-  var fichero = imagen("Imagen.png");
 
   var url =
-      Uri.http('placehold.co', '/todos/1');
+      Uri.http('placehold.co', '/600x400/png');
+  var response = await http.get(url);
+   if (response.statusCode == 200) {
+    var fichero = File("imagen.png");
 
+    fichero.writeAsBytes(response.bodyBytes);
+
+   } else{
+    print("Falta with status: ${response.statusCode}.");
+    }
 }
--- actividad8/test/actividad8_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:actividad8/actividad8.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
