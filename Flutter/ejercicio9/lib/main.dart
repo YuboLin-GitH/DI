3c868490353533b8402874bd484d4efc05e73f87
yubo <yubo.lin@educa.jcyl.es>
2025-10-29 23:24:10 +0100

    ej9

--- Flutter/ejercicio9/lib/main.dart
+++ Flutter/ejercicio9/lib/main.dart
@@ -9,10 +9,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Stack(
+          children: [
+            Image.network(
+              'https://placehold.co/600x300.png',
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              left: 32,
+              bottom: 32,
+              child: CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(
+                  'https://placehold.co/600x400/000000/FFFFFF.png',
+                ),
+              ),
+            ),
+            Positioned(
+              right: 40,
+              bottom: 40,
+              child: IconButton(
+                icon: Icon(Icons.edit, size: 32, color: Colors.blue),
+                onPressed: () {},
+                tooltip: 'editar',
+              ),
+            ),
+          ],
         ),
       ),
     );
