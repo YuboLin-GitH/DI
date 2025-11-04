3663cbfb378786106f490b052cce2c3b29405b52
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-11-04 13:20:00 +0100

    111

--- Flutter/ejercicio18_version_clase/lib/main.dart
+++ Flutter/ejercicio18_version_clase/lib/main.dart
@@ -9,29 +9,76 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text("Ejemolo1"),
-              trailing:Checkbox(value: false, onChanged: (bool? value){}),
-              
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: listaElementos()),
+    );
+  }
+}
+
+
+
+
+class listaElementos extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => listaElementosState();
+}
+
+
+
+
+class listaElementosState extends State<listaElementos> {
+  bool estado1 = false;
+   bool estado2 = false;
+    bool estado3 = false;
+
+    String tarea1 = "Ejemolo1";
+     String tarea2 = "Ejemolo2";
+      String tarea3 = "Ejemolo3";
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text(tarea1,
+                  style: TextStyle(
+                    color: estado1? Colors.grey : Colors.black ,
+                    decoration: estado1 ? TextDecoration.lineThrough : TextDecoration.none,
+                    ),
+                    
             ),
-             ListTile(
-              title: Text("Ejemolo1"),
-              trailing:Checkbox(value: false, onChanged: (bool? value){}),
-              
-            ),
-             ListTile(
-              title: Text("Ejemolo1"),
-              trailing:Checkbox(value: false, onChanged: (bool? value){}),
-              
-            ),
-          ],
+          trailing: Checkbox(value: estado1, onChanged: (bool? value) {
+              setState(() {
+                estado1 = !estado1;
+              });
+          }),
         ),
-      ),
+        ListTile(
+          title: Text(tarea2,
+                  style: TextStyle(
+                    color: estado2? Colors.grey : Colors.black ,
+                    decoration: estado2? TextDecoration.lineThrough : TextDecoration.none,
+                    ),),
+          trailing: Checkbox(value: estado2, onChanged: (bool? value) {
+             setState(() {
+                estado2 = !estado2;
+              });
+          }),
+        ),
+        ListTile(
+          title: Text(tarea3,
+                  style: TextStyle(
+                    color: estado3? Colors.grey : Colors.black ,
+                    decoration: estado3 ? TextDecoration.lineThrough : TextDecoration.none,
+                    ),),
+          trailing: Checkbox(value: estado3, onChanged: (bool? value) {
+             setState(() {
+                estado3 = !estado3;
+              });
+          }),
+        ),
+      ],
     );
   }
 }
