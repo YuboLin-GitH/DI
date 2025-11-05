4751f571cc272497b4e7d03c76879f83f4b7e5df
yubo <yubo.lin@educa.jcyl.es>
2025-11-05 22:40:46 +0100

    531

--- Flutter/ejercicio20_navegacion_basica/lib/main.dart
+++ Flutter/ejercicio20_navegacion_basica/lib/main.dart
@@ -9,10 +9,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const PantallaA(),
+    );
+  }
+}
+
+class PantallaA extends StatelessWidget{
+  const PantallaA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pantalla A"),),
+      body: Center(
+        child: ElevatedButton(onPressed:(){ 
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const PantallaB()),
+          );
+        }, child: Text("Ir al Pantalla B")),
+      ),
+    );
+  }
+
+}
+
+
+class PantallaB extends StatelessWidget {
+  const PantallaB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pantalla B")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Volver a Pantalla A"),
         ),
       ),
     );
