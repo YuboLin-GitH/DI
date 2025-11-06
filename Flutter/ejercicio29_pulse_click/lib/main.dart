d427e04095045bef4d61b3e1d150f3087674168d
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-11-06 14:16:29 +0100

    12312

--- Flutter/ejercicio29_pulse_click/lib/main.dart
+++ Flutter/ejercicio29_pulse_click/lib/main.dart
@@ -9,10 +9,83 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      routes: {
+      '/': (context) => PantallaA(), 
+      '/b': (context) => PantallaDosClick(),
+      '/c': (context) => PantallaDerecha(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class PantallaA extends StatelessWidget {
+  const PantallaA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {'/b': (context) => PantallaDosClick(),},
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: GestureDetector(
+            onDoubleTap: () {
+              Navigator.pushNamed(context, '/b');
+            },
+            onHorizontalDragEnd: (d) {
+              Navigator.pushNamed(context, '/c');
+            },
+
+            child: Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+              child: Center(child: Text('Toca aquí')),
+            ),
+
+
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class PantallaDosClick extends StatelessWidget {
+  const PantallaDosClick({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pantalla B")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Volver a Pantalla A"),
+        ),
+      ),
+    );
+  }
+}
+
+
+class PantallaDerecha extends StatelessWidget {
+  const PantallaDerecha({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pantalla derecho")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Volver a Pantalla A"),
         ),
       ),
     );
