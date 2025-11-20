99bfda19bcdffdd39b6e3edcf2c4a14a3ab4f656
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-11-20 13:40:48 +0100

    ada

--- Flutter/ejercicio33_gestionar_tarea_provider/lib/main.dart
+++ Flutter/ejercicio33_gestionar_tarea_provider/lib/main.dart
@@ -1,20 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final contador = context.watch<CounterProvider>().contador;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$contador',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    context.read<CounterProvider>().incrementar();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                Padding(padding: EdgeInsetsGeometry.all(15)),
+
+                  FloatingActionButton(
+                  onPressed: () {
+                    context.read<CounterProvider>().decrementar();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+              
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class CounterProvider extends ChangeNotifier {
+  int _contador = 0;
+
+  int get contador => _contador;
+
+  void incrementar() {
+    _contador++;
+    notifyListeners();
+  }
+
+  void decrementar() {
+    _contador--;
+    notifyListeners();
   }
 }
