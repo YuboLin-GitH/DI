59114b3761dc836f99c50ce89b8deaab4ed7a462
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-11-27 14:13:44 +0100

    12312

--- Flutter/ejemplo_nombre_edad_sqlite/lib/main.dart
+++ Flutter/ejemplo_nombre_edad_sqlite/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
@@ -11,13 +13,13 @@ void main() async {
   final database = await databaseFactory.openDatabase(dbPath);
   // Crear tabla de tareas si no existe
   await database.execute('''
- CREATE TABLE IF NOT EXISTS tasks (
+ CREATE TABLE IF NOT EXISTS nombre (
  id INTEGER PRIMARY KEY AUTOINCREMENT,
  nombre TEXT NOT NULL,
  edad INTEGER NOT NULL DEFAULT 0
  )
  ''');
-  runApp(Formulario(database: database,));
+  runApp(Formulario(database: database));
 }
 
 class Formulario extends StatefulWidget {
@@ -29,6 +31,28 @@ class Formulario extends StatefulWidget {
 }
 
 class _FormularioState extends State<Formulario> {
+final nombreController = TextEditingController();
+final edadController = TextEditingController();
+/*
+Future<void> _loadNombre() async {
+    final nombre = await widget.database.query('nombre');
+    setState(() {
+      _nombre = nombre;
+    });
+  }
+
+*/
+
+Future<void> _addNombre(String nombre, int edad) async {
+    await widget.database.insert('nombre', {'nombre': nombreController.text, 'edad': edad});
+    nombreController.clear();
+    edadController.clear();
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,9 +60,31 @@ class _FormularioState extends State<Formulario> {
         appBar: AppBar(title: Text("Formulario BD")),
         body: Column(
           children: [
-            TextField(decoration: InputDecoration(labelText: "Nombre")),
-            TextField(decoration: InputDecoration(labelText: "Contenido")),
-            ElevatedButton(onPressed: () {}, child: Text("Subir")),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: nombreController,
+                    decoration: InputDecoration(labelText: "Nombre")),
+                  TextField(
+                    controller: edadController,
+                    decoration: InputDecoration(labelText: "edad"),
+                  ),
+                  ElevatedButton(onPressed: () => _addNombre(nombreController.text, int.parse(edadController.text))
+                    
+                  , child: Text("Subir")),
+                ],
+              ),
+            ),
+
+            Expanded(child: 
+              //TODO: para muestra nombre - edad
+              ListView.builder(
+                itemBuilder: (context,index){
+
+                })
+            ),
           ],
         ),
       ),
