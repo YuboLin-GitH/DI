b8a12ed0ad06b4167342fbc453db0b942f1dc022
yubo <yubo.lin@educa.jcyl.es>
2025-12-03 20:52:20 +0100

    echo

--- Flutter/ejercicio34_parecido_examen_dificil_3_pantalla/lib/main.dart
+++ Flutter/ejercicio34_parecido_examen_dificil_3_pantalla/lib/main.dart
@@ -2,44 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
 import 'package:provider/provider.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   // Inicializar sqflite para escritorio
   sqfliteFfiInit();
   final databaseFactory = databaseFactoryFfi;
   // Ruta para la base de datos
-  final dbPath = join(await databaseFactory.getDatabasesPath(), 'nombre.db');
+  final dbPath = join(await databaseFactory.getDatabasesPath(), 'persona.db');
   final database = await databaseFactory.openDatabase(dbPath);
   // Crear tabla de nombre si no existe
   await database.execute('''
- CREATE TABLE IF NOT EXISTS nombre (
+ CREATE TABLE IF NOT EXISTS persona (
  id INTEGER PRIMARY KEY AUTOINCREMENT,
  nombre TEXT NOT NULL,
  edad INTEGER NOT NULL DEFAULT 0
  )
  ''');
+
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => BBDDProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => BBDDProvider(database: database),
+        ),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+
       child: MyApp(),
     ),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = context.watch<ThemeProvider>();
     return MaterialApp(
       title: 'Flutter 34 de Yubo',
       debugShowCheckedModeBanner: false, // quitar Debug
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.light().copyWith(
+        textTheme: Theme.of(
+          context,
+        ).textTheme.apply(fontSizeFactor: themeProvider.fontSize/ 12.0 ),
       ),
+      darkTheme: ThemeData.dark().copyWith(
+        textTheme: Theme.of(
+          context,
+        ).textTheme.apply(fontSizeFactor: themeProvider.fontSize / 12.0 ),
+      ),
+      themeMode: themeProvider.themeMode,
+
       home: const HomeNavScreen(),
     );
   }
@@ -96,35 +111,53 @@ class _HomeNavScreenState extends State<HomeNavScreen> {
 
 // PANTALLAS Generar
 
+class Persona {
+  String nombre;
+  int edad;
 
-
-
-class BBDDProvider extends ChangeNotifier {
-  int _contador = 0;
-
-  int get contador => _contador;
-
-  void incrementar() {
-    _contador++;
-    notifyListeners();
-  }
-
-  void decrementar() {
-    _contador--;
-    notifyListeners();
-  }
-
-
-
-
-  
+  Persona(this.nombre, this.edad);
 }
 
+class BBDDProvider extends ChangeNotifier {
+  final Database database;
 
+  List<Map<String, dynamic>> _personaLista = [];
+  List<Map<String, dynamic>> get personaLista => _personaLista;
 
+  BBDDProvider({required Database database}) : database = database {
+    loadPersona();
+  }
 
+  Future<void> loadPersona() async {
+    final personas = await database.query('persona');
+    _personaLista = personas;
+    notifyListeners();
+  }
 
+  Future<void> addPersona(String nombre, int edad) async {
+    await database.insert('persona', {'nombre': nombre, 'edad': edad});
+    await loadPersona();
+  }
 
+  Future<void> togglePersona(String nombre, int edad) async {
+    await database.update(
+      'persona',
+      {'edad': edad},
+      where: 'nombre = ?',
+      whereArgs: [nombre],
+    );
+    loadPersona();
+  }
+
+  Future<void> deletePersona(String nombre, int edad) async {
+    await database.delete(
+      'persona',
+      where: 'nombre = ? and  edad = ?',
+      whereArgs: [nombre, edad],
+    );
+    loadPersona();
+  }
+}
 
 class FormularioScreen extends StatefulWidget {
   const FormularioScreen({super.key});
@@ -134,56 +167,13 @@ class FormularioScreen extends StatefulWidget {
 }
 
 class _FormularioScreenState extends State<FormularioScreen> {
- 
-
- List<Map<String, dynamic>> _nombre = [];
-
   final nombreController = TextEditingController();
   final edadController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    _loadNombre();
-  }
-
-  Future<void> _loadNombre() async {
-    final nombre = await widget.database.query('nombre');
-    setState(() {
-      _nombre = nombre;
-    });
-  }
-
-  Future<void> _addNombre(String nombre, int edad) async {
-    await widget.database.insert('nombre', {'nombre': nombre, 'edad': edad});
-    nombreController.clear();
-    edadController.clear();
-    _loadNombre();
-  }
-
-
-   Future<void> _toggleNombre(String nombre, int edad) async {
-    await widget.database.update(
-      'nombre',
-      {'edad' : edad},
-      where: 'nombre = ?',
-      whereArgs: [nombre],
-    );
-    _loadNombre();
-  }
-
-
-  Future<void> _deleteNombre(String nombre, int edad) async {
-    await widget.database.delete('nombre', 
-                                  where: 'nombre = ? and  edad = ?', 
-                                  whereArgs: [nombre, edad]);
-    _loadNombre();
-  }
-
-
-
-  @override
   Widget build(BuildContext context) {
+    final bbddProvider = Provider.of<BBDDProvider>(context);
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -204,28 +194,42 @@ class _FormularioScreenState extends State<FormularioScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ElevatedButton(
-                        onPressed: () => _addNombre(
-                          nombreController.text,
-                          int.parse(edadController.text),
-                        ),
+                        onPressed: () {
+                          final nombre = nombreController.text;
+                          final edad = int.tryParse(edadController.text) ?? 0;
+                          bbddProvider.addPersona(nombre, edad);
+
+                          nombreController.clear();
+                          edadController.clear();
+                        },
                         child: Text("Agregar"),
                       ),
 
                       // modificar edad
                       ElevatedButton(
-                        onPressed: () => _toggleNombre(
-                          nombreController.text,
-                          int.parse(edadController.text),
-                        ),
+                        onPressed: () {
+                          bbddProvider.togglePersona(
+                            nombreController.text,
+                            int.parse(edadController.text),
+                          );
+
+                          nombreController.clear();
+                          edadController.clear();
+                        },
                         child: Text("Modificar Edad"),
                       ),
 
                       // Eliminar cuyo nombre y edad erar mismo que introducido
                       ElevatedButton(
-                        onPressed: () => _deleteNombre(
-                          nombreController.text,
-                          int.parse(edadController.text),
-                        ),
+                        onPressed: () {
+                          bbddProvider.deletePersona(
+                            nombreController.text,
+                            int.parse(edadController.text),
+                          );
+
+                          nombreController.clear();
+                          edadController.clear();
+                        },
                         child: Text("Eliminar"),
                       ),
                     ],
@@ -240,16 +244,31 @@ class _FormularioScreenState extends State<FormularioScreen> {
   }
 }
 
-// PANTALLAS Feed
+// PANTALLAS View
 
 class ViewScreen extends StatelessWidget {
   const ViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bbddProvider = Provider.of<BBDDProvider>(context);
+    final personas = bbddProvider.personaLista;
+
     return Scaffold(
       appBar: AppBar(title: Text("View")),
-      body: Center(child: Text("No hay datos")),
+      body: personas.isEmpty
+          ? Center(child: Text("No hay datos"))
+          : ListView.builder(
+              itemCount: personas.length,
+              itemBuilder: (context, index) {
+                final persona = personas[index];
+                return ListTile(
+                  leading: Text("${persona['id']}"),
+                  title: Text(persona['nombre']),
+                  trailing: Text("${persona['edad']} años"),
+                );
+              },
+            ),
     );
   }
 }
@@ -265,12 +284,11 @@ class OptionsViewOpenDirection extends StatefulWidget {
 }
 
 class _OptionsViewOpenDirectionState extends State<OptionsViewOpenDirection> {
-  var estadoswitch = false;
-  String terminos = "espanol";
-  late double valueLinea = 0;
-
   @override
   Widget build(BuildContext context) {
+    final themeProvider = context.watch<ThemeProvider>();
+    final esClaro = context.watch<ThemeProvider>().esClaro;
+    final currentScale = themeProvider.fontSize ;
     return Scaffold(
       body: ListView(
         children: [
@@ -291,14 +309,8 @@ class _OptionsViewOpenDirectionState extends State<OptionsViewOpenDirection> {
               leading: Icon(Icons.dark_mode),
               title: Text("Tema claro"),
               trailing: Switch(
-                onChanged: (estado) {
-                  setState(() {
-                    estadoswitch = estado;
-                    if (estadoswitch) {
-                    } else {}
-                  });
-                },
-                value: estadoswitch,
+                onChanged: (_) => context.read<ThemeProvider>().toggleTheme(),
+                value: esClaro,
               ),
               contentPadding: EdgeInsets.all(10),
             ),
@@ -318,13 +330,60 @@ class _OptionsViewOpenDirectionState extends State<OptionsViewOpenDirection> {
               borderRadius: BorderRadius.circular(20),
             ),
             child: ListTile(
-              title: Text("Tamaño de texto: 1.0"),
-
+              title: Text("Tamaño de texto: ${currentScale.toStringAsFixed(1)}"),
+              subtitle: Slider(
+                min: 10,
+                max: 14,
+                divisions: 4,
+                value: currentScale,
+                onChanged: (value) {
+                  themeProvider.setFontSizeScale(value);
+                },
+              ),
               contentPadding: EdgeInsets.all(10),
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class ThemeProvider extends ChangeNotifier {
+  bool _esClaro = false;
+  bool get esClaro => _esClaro;
+  ThemeMode get themeMode => _esClaro ? ThemeMode.light : ThemeMode.dark;
+
+  static const String _fontSizeKey = 'fontSize';
+  double _fontSize  = 12.0;
+
+  double get fontSize => _fontSize;
+
+  ThemeProvider() {
+    _loadFromPrefs();
+  }
+
+  Future<void> _loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _esClaro = prefs.getBool('esClaro') ?? false;
+
+     _fontSize = prefs.getDouble(_fontSizeKey) ?? 12.0;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    _esClaro = !_esClaro;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('esClaro', _esClaro);
+  }
+
+  Future<void> setFontSizeScale(double newScale) async {
+    _fontSize  = newScale.clamp(10, 14);
+
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble(_fontSizeKey, _fontSize );
   }
 }
